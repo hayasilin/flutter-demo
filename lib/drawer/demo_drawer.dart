f6d43567618eefa098b2d730e79c03517a419a37
hayasilin <harvey.hayasi@gmail.com>
2021-01-30 21:16:16 +0800

    remove image that will cause error

--- lib/drawer/demo_drawer.dart
+++ lib/drawer/demo_drawer.dart
@@ -10,7 +10,6 @@ class DemoDrawer extends StatelessWidget {
     return Drawer(
       child: MediaQuery.removePadding(
         context: context,
-        //移除抽屉菜单顶部默认留白
         removeTop: true,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -19,17 +18,8 @@ class DemoDrawer extends StatelessWidget {
               padding: const EdgeInsets.only(top: 38.0),
               child: Row(
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: ClipOval(
-                      child: Image.asset(
-                        "imgs/avatar.png",
-                        width: 80,
-                      ),
-                    ),
-                  ),
                   Text(
-                    "Wendux",
+                    "Demo Drawer",
                     style: TextStyle(fontWeight: FontWeight.bold),
                   )
                 ],
